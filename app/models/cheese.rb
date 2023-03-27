4b618493e8d5576c44e2ae973e0ff5de6c370cd2
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <cvsenes@gmail.com>
2023-03-27 17:54:14 +0200

    model review ok

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,2 +1,23 @@
 class Cheese < ApplicationRecord
+  has_many :bookmarks
+  has_many :reviews
+  has_many :recipes
+  has_many :forbidden
+
+  validates :name, presence: true
+  validates :affinage, presence: true
+  validates :lait, presence: true
+  validates :pate, presence: true
+  validates :saison, presence: true
+  validates :category, presence: true
+  validates :picture_url, presence: true
+  validates :description_1, presence: true
+  validates :description_2, presence: true
+  validates :filter_pregnant, presence: true
+  validates :filter_vache, presence: true
+  validates :filter_chevre, presence: true
+  validates :filter_brebis, presence: true
+  validates :filter_AOP, presence: true
+  validates :wine, presence: true
+  validates :address, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :cheeses, only: [:index, :map, :region, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:index]
+  resources :users, only: [:edit, :update]
 end
