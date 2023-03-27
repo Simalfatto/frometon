8d1fdb13f21a38375545834532319efb81101e5f
(Valentin Styza) <styzavalentin@gmail.com>
2023-03-27 17:52:18 +0200

    controllers bookmarks et reviews avec vues associées

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :cheese
   belongs_to :user
+
+  AUTHORIZED_RATINGS = (0..5)
+
+  validates :review_content, presence: true
+  validates :review_rating, inclusion: { in: AUTHORIZED_RATINGS }
 end
