b4b6074a4a8fc08eff3e46e6ba1a397f35c0bedc
Simon Malfatto <simon.malfatto@gmail.com>
2023-03-28 17:20:06 +0200

    formulaire in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,34 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :result ]
 
+  def home
+
+  end
+
   def result
-    @cheese = Cheese.order("RANDOM()").limit(1).first
+    if current_user
+      cheeses = Cheese.all
+      test = []
+      vaches = cheeses.where(lait: "vache")
+      current_user.score_search_vache.times do
+        vache = vaches.sample
+        test.push(vache)
+      end
+
+      brebis = cheeses.where(lait: "brebis")
+      current_user.score_search_brebis.times do
+        brebi = brebis.sample
+        test.push(brebi)
+      end
+
+      chevres = cheeses.where(lait: "chèvre")
+      current_user.score_search_chevre.times do
+        chevre = chevres.sample
+        test.push(chevre)
+      end
+      @cheese = test.sample
+    else
+      @cheese = Cheese.order("RANDOM()").limit(1).first
+    end
   end
 end
