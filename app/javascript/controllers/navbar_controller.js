59930db927d0b07dc66632b44087d325a8ec2033
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <cvsenes@gmail.com>
2023-03-29 17:45:58 +0200

    pas fini

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,9 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["value"]
+  static targets = ["value", "logo"]
 
   connect() {
     this.valueTarget.value = ""
+  }
+  display() {
+    this.logoTarget.classList.add("d-none");
   }
 }
