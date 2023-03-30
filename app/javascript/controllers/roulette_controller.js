5bd54f8cf2e24d34895768f1089e5ca382151fee
Simon Malfatto <simon.malfatto@gmail.com>
2023-03-30 12:08:08 +0200

    work in progress

--- app/javascript/controllers/roulette_controller.js
+++ app/javascript/controllers/roulette_controller.js
@@ -5,7 +5,12 @@ export default class extends Controller {
   static targets = ["parameters", "button", "btn2"]
 
   display() {
-    console.log("inside ok")
+    this.parametersTarget.classList.toggle("d-none")
+    this.buttonTarget.classList.toggle("d-none")
+    this.btn2Target.classList.toggle("d-none")
+  }
+
+  close() {
     this.parametersTarget.classList.toggle("d-none")
     this.buttonTarget.classList.toggle("d-none")
     this.btn2Target.classList.toggle("d-none")
